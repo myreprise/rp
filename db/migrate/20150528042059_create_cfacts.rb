22d21e369b6397f0a78947dba3879bd9415ef850
myreprise <brettlill@gmail.com>
2015-05-30 17:31:23 +0800

    first commit

--- db/migrate/20150528042059_create_cfacts.rb
+++
@@ -1,9 +0,0 @@
-class CreateCfacts < ActiveRecord::Migration
-  def change
-    create_table :cfacts do |t|
-      t.string :fact_category
-      t.string :fact_title
-      t.timestamps null: false
-    end
-  end
-end
